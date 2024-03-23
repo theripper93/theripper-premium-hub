d92f6a53e18df78388c2add0459106e630b9a5cd
theripper93 <tsplab@gmail.com>
2024-03-23 19:21:43 +0100

    a

--- scripts/main.js
+++ scripts/main.js
@@ -5,16 +5,18 @@ Hooks.on("ready", async () => {
     if (game.settings.get("theripper-premium-hub", "cleanHeaderButtons")) {
         libWrapper.register("theripper-premium-hub", "Application.prototype._renderOuter", async function (wrapped, ...args) {
             const html = await wrapped(...args);
-            const header = html.find(".window-header")[0];
-            if (!header) return;
-            header.querySelectorAll(".header-button").forEach((button) => {
-                const innerText = button.innerText.trim();
-                if (innerText) {
-                    button.dataset.tooltip = button.innerText;
-                    button.dataset.tooltipDirection = "UP";
-                    button.innerHTML = button.innerHTML.replace(innerText, "");
-                }
-            });
+            setTimeout(() => {
+                const header = html.find(".window-header")[0];
+                if (!header) return;
+                header.querySelectorAll(".header-button").forEach((button) => {
+                    const innerText = button.innerText.trim();
+                    if (innerText) {
+                        button.dataset.tooltip = button.innerText;
+                        button.dataset.tooltipDirection = "UP";
+                        button.innerHTML = button.innerHTML.replace(innerText, "");
+                    }
+                });
+            }, 1);
             return html;
         });
     }
